2134691dbb8dc1416889d40ac31754a6e1fe76a5
t16cs054 <t16cs054@cw06.kki.yamanashi.ac.jp>
2018-10-10 17:22:23 +0900

    sample

---
+++ Sample.py
@@ -0,0 +1,10 @@
+'''
+Created on 2018/10/10
+
+@author: t16cs054
+'''
+print('Hello Python world!')
+print('Hello Python world!')
+print('Hello Python world!')
+print('Hello Python world!')
+print('Hello Python world!')
